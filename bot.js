bba6ec527b35f123acfdc6aa6976bb561bd2a8c2
bedirhan59 <40547748+bedirhan59@users.noreply.github.com>
2018-06-25 00:07:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('bağlandım!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === 'bedirhan') {
+    	message.channel.send('Kurucu');
   	}
 });
 
@@ -15,11 +15,7 @@ client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
- if (message.content === prefix + "iletişim") {
-        message.channel.sendMessage(`Discord https://discord.gg/aAuKP6Q 
-                                     facebook https://www.facebook.com/groups/237652310371272/
-                                    `)
-        }
+
   
 });
 // THIS  MUST  BE  THIS  WAY
