00c5635960e7fa5e73f33e4dbe4c948ced39b72e
bedirhan59 <40547748+bedirhan59@users.noreply.github.com>
2018-06-25 00:06:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,28 +15,11 @@ client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(prefix) !== 0) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
- 
- if(command === "temizle") {
-    if(!message.member.roles.some(r=>["kurucu", "admin", "rehber"].includes(r.name)) )
-      return message.reply("Üzgünüm, Admin veya Moderator rolüne sahip olmalısın.");
-
-    const deleteCount = parseInt(args[0], 10);
-
-    if(!deleteCount || deleteCount < 2 || deleteCount > 101)
-      return message.reply("Lütfen 2 ile 100 arası sayı giriniz.");
-
-    const fetched = await message.channel.fetchMessages( { count : deleteCount } );
-    message.channel.bulkDelete(deleteCount)
-
-
-  }
-});
+ if (message.content === prefix + "iletişim") {
+        message.channel.sendMessage(`Discord https://discord.gg/aAuKP6Q 
+                                     facebook https://www.facebook.com/groups/237652310371272/
+                                    `)
+        }
   
 });
 // THIS  MUST  BE  THIS  WAY
