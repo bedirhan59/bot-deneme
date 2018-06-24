7afbc92186f1893e37ad0535f842f9e8819755b1
bedirhan59 <40547748+bedirhan59@users.noreply.github.com>
2018-06-25 00:00:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,28 @@ client.on('message', message => {
     	message.reply('BONG!');
   	}
 });
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(prefix) !== 0) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+ if(command === "temizle") {
+    if(!message.member.roles.some(r=>["kurucu", "admin", "rehber"].includes(r.name)) )
+      return message.reply("Üzgünüm, Admin veya Moderator rolüne sahip olmalısın.");
+
+    const deleteCount = parseInt(args[0], 10);
+
+    if(!deleteCount || deleteCount < 2 || deleteCount > 101)
+      return message.reply("Lütfen 2 ile 100 arası sayı giriniz.");
+
+    const fetched = await message.channel.fetchMessages( { count : deleteCount } );
+    message.channel.bulkDelete(deleteCount)
+
+
+  }
+  });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
