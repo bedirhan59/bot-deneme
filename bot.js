e88e7cbb1b9da7d1e41695ed11469decf50b9d46
bedirhan59 <40547748+bedirhan59@users.noreply.github.com>
2018-06-25 13:07:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('bağlandım!');
 });
 
 client.on('message', message => {
@@ -15,7 +15,10 @@ client.on('message', message => {
     if (message.content === 'mert') {
     	message.channel.send('benim 1 tane kardeşim');
   	}
-
+   client.on('message', message => {
+    if (message.content === 'bot') {
+    	message.channel.send('şuanda beta da gelişmeye devam ediyor');
+  	}
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
