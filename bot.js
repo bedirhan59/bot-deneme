ccebdb04d308099fae8ac25e38eb79a5ce6a31be
bedirhan59 <40547748+bedirhan59@users.noreply.github.com>
2018-06-25 13:32:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,12 @@ client.on('message', message => {
     if (message.content === 'mert') {
     	message.channel.send('benim 1 tane kardeşim');
   	}
-
+   client.on('message', message => {
+    if (message.content === 'bot') {
+    	message.channel.send('bot bakım aşamasındadır');
+  	}
 });
+
+ });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
