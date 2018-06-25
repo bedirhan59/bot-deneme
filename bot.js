1a71989b195dabbf64eb9de84f6dd1edca0f03ed
bedirhan59 <40547748+bedirhan59@users.noreply.github.com>
2018-06-25 13:35:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,15 @@ client.on('message', message => {
     	message.channel.send('bot bakım aşamasındadır');
   	}
 });
-
+ client.on('message', message => {
+    if (message.content === 'aşk') {
+    	message.channel.send('Ey Aşk sen nelere kadirsin…
+An olur göz görmez olur sana düşünce, an gelir bir el uzanmaz yere düşünce…
+An gelir ruhumu alır sarmalarsın sıcacık bakışlara, an olur gark edersin son bulmaz kara kışlara…
+Gün gelir bir kıvılcım olur düşersin yüreklere, gün gelir büyürsün yangın olup talan edersin…
+Gün gelip yol alır senle yürekler umudun maviliklerinde, gün gelir alır yutarsın, kaybolur gider sevdalılar karanlığının derinliklerinde…');
+  	}
+});
  });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
